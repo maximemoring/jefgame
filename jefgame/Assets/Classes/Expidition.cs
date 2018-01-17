e76833fcb210f128f151416a9328a25b3f34b640
Maxime Morin-Gravel <maximemoring@hotmail.com>
2018-01-16 21:36:56 -0500

    Added Expidition and Settlement classes with very basic setup

---
+++ jefgame/Assets/Classes/Expidition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expidition : MonoBehaviour {
+    List<Habitant> expeditionists;
+    short leaderID; // Relates to the ID in the List
+
+	// Use this for initialization
+	void Start (Habitant[] selectedExpeditionists, short selectedLeaderID) {
+        expeditionists = new List<Habitant>(selectedExpeditionists);
+        leaderID = selectedLeaderID;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ jefgame/Assets/Classes/Settlement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settlement : MonoBehaviour {
+    List<Habitant> habitants;
+
+	// Use this for initialization
+	void Start () {
+        habitants = new List<Habitant>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
